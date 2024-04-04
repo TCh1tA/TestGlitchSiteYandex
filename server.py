cb37948df27be32be8cf61f5493cae5924382c99
TCh1tA <shums-demid@yandex.ru>
2024-04-04 20:16:20 +0300

    Second try

--- server.py
+++ server.py
@@ -1,1 +1,17 @@
-print('Hello world')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Миссия колонизации Марса'
+
+
+@app.route('/index')
+def yabloki():
+    return 'И на Марсе будут яблони цвести!'
+
+
+if __name__ == "__main__":
+    app.run('127.0.0.1', port=8080)
